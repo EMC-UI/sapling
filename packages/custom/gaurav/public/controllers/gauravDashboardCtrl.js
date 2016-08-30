da0448feb106cc4b1329d53ba1d798a8f1729a8d
Duc Ho <duc.ho@emc.com>
2016-08-30 09:44:50 -0600

    remove gaurav dashboard example

--- packages/custom/gaurav/public/controllers/gauravDashboardCtrl.js
+++ packages/custom/gaurav/public/controllers/gauravDashboardCtrl.js
@@ -4,45 +4,7 @@
   /* jshint -W098 */
 
   function GauravDashboardController($scope, GauravService, CommonService, $state) {
-
-    $scope.totalTestCount;
-    $scope.testResults = [];
-
-    $scope.dashboardClick = function() {
-      $state.go('gaurav');
-    };
-    $scope.labels = [];
-    $scope.series = ['Passed', 'Failed', 'Skipped'];
-    $scope.data = [
-      [], //passed data points for each testResult
-      [], //failed data points for each testResult
-      []  //skipped data points for each testResult
-    ];
-
-    function getTestResultsSuccess(response) {
-      // console.log(JSON.stringify(result, null, 2));
-      var testResults = response.data;
-      $scope.labels = [];
-      $scope.data[0] =[];
-      $scope.data[1] =[];
-      $scope.data[2] =[];
-      testResults.forEach(function(testResult) {
-        $scope.labels.push(testResult.suite.name);
-        $scope.data[0].push(parseInt(testResult.passed));
-        $scope.data[1].push(parseInt(testResult.failed));
-        $scope.data[2].push(parseInt(testResult.skipped));
-      });
-      $scope.testResults = response.data;
-    }
-
-    function getTestResultsFailed(err) {
-      console.log('***getTestResults() Failed!');
-      console.log(JSON.stringify(err, null, 2));
-    }
-
-    CommonService.getTestResults()
-      .then(getTestResultsSuccess, getTestResultsFailed);
-
+    
 
   } //end of controller
 
